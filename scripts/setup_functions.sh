27051c3c8b6cf5512a696f15900ca34ee7841738
Garrett Gibo <garrett.gibo@gmail.com>
2020-08-28 00:04:40 -0700

    Clean setup; add template functions

---
+++ scripts/setup_functions.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Helper setup functions
+#
+
+function setup_zsh {
+  echo "Setting up zsh";
+  # TODO:
+  # - install zsh
+  #   - check platform i.e., debian, arch
+  # - install oh-my-zsh
+  # - link .zshenv and .zshrc to $XDG_CONFIG location
+}
+
+function setup_neovim {
+  echo "Setting up neovim";
+  # TODO:
+  # - install neovim
+  # - link init.vim
+}
+
+function setup_tmux {
+  echo "Setting up tmux";
+  # TODO: 
+  # install tmux
+  # - link tmux.conf
+}
--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@
 
 # Source necessary functions
 source ./scripts/multiselect.sh
-source ./scripts/
+source ./scripts/setup_functions.sh
 
-OPTIONS=("Zsh" "Neovim" "Tmux")
+OPTIONS=("zsh" "neovim" "tmux")
 OPTIONS_STRING=$(IFS=\; ; echo "${OPTIONS[*]}")
 
 multiselect SELECTED "$OPTIONS_STRING"
@@ -16,40 +16,9 @@ multiselect SELECTED "$OPTIONS_STRING"
 for i in "${!SELECTED[@]}"; do
   if [ "${SELECTED[$i]}" == "true" ]; then
     case ${OPTIONS[$i]} in
-      "Zsh") 
-        echo "Setting Up Zsh"
-	echo "TODO: Call zsh setup";;
-      "Neovim") 
-        echo "Setting Up Neovim"
-	echo "TODO: Call Neovim setup";;
-      "Tmux") 
-        echo "Setting Up Tmux"
-	echo "TODO: Call zsh setup";;
+      "zsh") setup_zsh;;
+      "neovim") setup_neovim;;
+      "tmux") setup_tmux;;
     esac
   fi
 done
-
-# Link neovim init.vim
-# nvimHome=$HOME/.config/nvim
-# [ ! -d $nvimHome ] && echo "Creating .config/nvim" && mkdir $nvimHome
-# [ -f $nvimHome/init.vim ] && echo "Removing previous init" && rm $nvimHome/init.vim
-# ln nvim/init.vim $nvimHome/init.vim && echo "Linking init.vim"
-
-#===============================
-# ZSH Setup
-#===============================
-# echo "Install/setup ZSH? [Y,n]"
-# read input
-# if [[ $input == "Y" || $input == "y" || ]]
-# ZSHHOME=$HOME/.config/zsh
-# [ ! -d $ZSHHOME ] && echo "Adding zsh config directory at $ZSHHOME" && mkdir $ZSHHOME
-# [ -f $ZSHHOME/.zshrc ] && echo "Removing previous .zshrc" && rm $ZSHHOME/.zshrc
-# [ -f $HOME/.zshenv ] && echo "Removing previous .zshenv" && rm $HOME/.zshenv
-# ln zsh/zshenv $HOME/.zshenv && echo "Linking .zshenv"
-# ln zsh/zshrc $ZSHHOME/.zshrc && echo "Linking .zshrc"
-
-# Link zsh stuff
-# [ -f $Home/.tmux.conf ] && echo "Removing previous tmux.conf" && rm $Home/.tmux.conf
-# ln tmux/tmux.conf $HOME/.tmux.conf && echo "Linking tmux.conf"
-
-
